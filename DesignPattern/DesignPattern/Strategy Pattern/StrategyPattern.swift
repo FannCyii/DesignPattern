721ab51777e4cb64afba112cf35447210df7d3d1
FannC <chengkivan@gmail.com>
2019-11-18 09:30:35 +0800

    Update StrategyPattern.swift

--- DesignPattern/DesignPattern/Strategy Pattern/StrategyPattern.swift
+++ DesignPattern/DesignPattern/Strategy Pattern/StrategyPattern.swift
@@ -28,8 +28,10 @@ import Foundation
  ConcreteStrategy 具体策略角色
 
 
-疑惑： 为何能消除if else 语句？ 看很多例子只不过是将context中的ifelse 移动到工程中，或者另一个管理类（必定绕不开对给定条件的筛选，
-就是需要一个类似ifelse的过程），例如使用map<key, strategy>这种方式，是一种筛选过程，在某些情况下实现起来未必比ifelse好吧？
+疑惑： 为何能消除if else 语句？ 看很多例子只不过是将context中的ifelse 移动到工厂类中，或者另一个管理类（必定绕不开对给定条件的筛选，
+就是需要一个类似ifelse的过程），例如使用map<key, strategy>这种方式，是一种筛选过程，在某些情况下实现起来未必比ifelse好吧？所以应该说是为了代码的
+可维护性和可读性，不能把实现都房子ifelse中，而是让ifelse代码更加简洁易懂，可以结合strategy模式，来达到这个效果，同时ifelse也可以通过算法命中率来提供
+命中效率上的优化。
  
  */
 
