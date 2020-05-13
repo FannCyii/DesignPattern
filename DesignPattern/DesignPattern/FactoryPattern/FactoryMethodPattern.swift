85cc525d7ca822c4b32a4d1996f4d51e1d868537
FannC <chengkivan@gmail.com>
2020-05-13 10:01:50 +0800

    Update FactoryMethodPattern.swift

--- DesignPattern/DesignPattern/FactoryPattern/FactoryMethodPattern.swift
+++ DesignPattern/DesignPattern/FactoryPattern/FactoryMethodPattern.swift
@@ -16,7 +16,7 @@ import Foundation
 
  Define an interface for creating an object, but let subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses.
  
- 定义一个用于创建对象的接口，让子类决定实例化哪一个类。工厂方法使一个累的实例化延迟到其子类
+ 定义一个用于创建对象的接口，让子类决定实例化哪一个类。工厂方法使一个类的实例化延迟到其子类
  */
 
 
